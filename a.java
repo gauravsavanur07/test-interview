fc011506915c5968f11cb22a91a30a2fad47c4a7
Gaurav Savanur <31487324+gauravsavanur07@users.noreply.github.com>
2019-02-28 10:05:57 +0000

    Update a.java

--- a.java
+++ a.java
@@ -8,35 +8,38 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-        int n;
-        n= scan.nextInt();
-        ArrayList[] list = new ArrayList[20002];
-        
-        for(int i=1;i<n;i++){
-            int x =scan.nextInt();
-            list[i] = new ArrayList();
-             for(int j=1;j<=x;j++){
-                    int val = scan.nextInt();
-                    list[i].add(val); 
-             }
-           
+       Scanner scan = new Scanner(System.in);
+      ArrayList[] list= new ArrayList[20002];
+      int n;
+      n=scan.nextInt();
+      for(int i=1;i<=n;i++)
+      {
 
-        }
-        int q = scan.nextInt();
-        for(int i=1;i<=q;i++){
-            int x,y;
-            x =scan.nextInt();
-            y = scan.nextInt();
-            try
+          list[i]=new ArrayList();
+          int x=scan.nextInt();
+
+          for(int j=1;j<=x;j++)
+          {
+              int val=scan.nextInt();
+              list[i].add(val);
+
+          }
+      }
+      int q=scan.nextInt();
+
+      for(int i=1;i<=q;i++)
+      {
+              int x,y;
+              x=scan.nextInt();
+              y=scan.nextInt();
+              try
               {
                 System.out.println(list[x].get(y-1));
               }catch(Exception e)
               {
                   System.out.println("ERROR!");
               }
-
-        }
+      }
     }
 }
 
